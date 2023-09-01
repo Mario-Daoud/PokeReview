8c6b016249b57a50d206ffda91a8247f0992d5b5
mario <r0895582@student.vives.be>
2023-09-01 14:23:14 +0200

    add: Models

---
+++ PokeReview/Models/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace PokeReview.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name{ get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
